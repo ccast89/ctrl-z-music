00288c15d119c06818510431af72a72034cfdf39
ccast89 <carlacast09@gmail.com>
2026-09-05 19:23:00 -0300

    Se actualiza el protected routes para que funcione distinto con un usario normal y el administrador. También se modifica el boton de acceso en el nav para que muestre -cerrar sesion- cuando corresponda

---
+++ src/routes/ProtectedRoutes.tsx
@@ -0,0 +1,29 @@
+import { useContext, type ReactNode } from "react";
+import { Navigate } from "react-router";
+import { AuthContext } from "../context/AuthContext";
+
+interface ProtectedRoutesProps {
+  children: ReactNode;
+}
+
+function ProtectedRoutes({ children }: ProtectedRoutesProps) {
+  const auth = useContext(AuthContext);
+
+  if (!auth) {
+    return null;
+  }
+
+  if (!auth.user) {
+    return <Navigate to="/login" />;
+  }
+
+  if (auth.user.rol !== "admin") {
+    return <Navigate to="/" />;
+  }
+
+  return children;
+
+  return children;
+}
+
+export default ProtectedRoutes;
